4b56c92ee64fc20e4e415c17d43a3d859765714a
phillip-kemper <phillip-kemper@gmx.net>
2023-12-02 11:45:15 +0100

    day 2 star 2

--- aoc23/src/day2/main.rs
+++ aoc23/src/day2/main.rs
@@ -29,19 +29,22 @@ fn main() {
     let chunks: Vec<&str> = contents.split("\n").collect();
 
     let mut sum = 0;
+    let mut sum_of_powers = 0;
     for (game,chunk) in chunks.iter().enumerate() {
+        sum_of_powers += get_power_of_game(chunk);
         if are_all_games_possible(chunk, game_config.clone()) {
-            println!("game {} is possible", game+1);
+            // println!("game {} is possible", game+1);
             sum += game+1;
         } else {
-            println!("game {} is not possible", game+1);
+            // println!("game {} is not possible", game+1);
         }    
     }
-    println!("sum: {}", sum)
+    println!("sum: {}", sum);
+    println!("sum_of_powers: {}", sum_of_powers);
 }
 
 fn are_all_games_possible(chunk: &str, game_config: Game) -> bool {
-    println!("checking chunk: {}", chunk);
+    // println!("checking chunk: {}", chunk);
     let current_game_vec = process_chunk(chunk);
     // println!("current_game_vec: {:?}", current_game_vec);
     for game in current_game_vec {
@@ -69,13 +72,13 @@ fn process_chunk(chunk: &str) -> Vec<Game>  {
 }
 
 fn compare_game_with_game_config(game: Game, game_config: Game) -> bool {
-    println!("game: {:?}", game);
-    println!("game_config: {:?}", game_config);
+    // println!("game: {:?}", game);
+    // println!("game_config: {:?}", game_config);
     if game.red <= game_config.red && game.green <= game_config.green && game.blue <= game_config.blue {
-        println!("true");
+        // println!("true");
         true
     } else {
-        println!("false");
+        // println!("false");
         false
     }
 
@@ -87,7 +90,7 @@ fn subgame_from_chunk(game_str: &str) -> Game {
     let dices = game_str.trim().split(",").collect::<Vec<&str>>();
 
     for dice in dices {
-        println!("dice: {}", dice);
+        // println!("dice: {}", dice);
         let regex_red = Regex::new(r"[0-9]+ red").unwrap();
         let regex_blue = Regex::new(r"[0-9]+ blue").unwrap();
         let regex_green = Regex::new(r"[0-9]+ green").unwrap();
@@ -95,17 +98,39 @@ fn subgame_from_chunk(game_str: &str) -> Game {
         let digit: u32 = dice.chars().filter(|c| c.is_digit(10)).collect::<String>().parse().unwrap();
 
         if regex_red.is_match(dice) {
-            println!("red: {}", digit);
+            // println!("red: {}", digit);
             game.red = digit;
         } else if regex_blue.is_match(dice) {
-            println!("blue: {}", digit);
+            // println!("blue: {}", digit);
             game.blue = digit;
         } else if regex_green.is_match(dice) {
-            println!("green: {}", digit);
+            // println!("green: {}", digit);
             game.green = digit;
         }
 
     }
     game
+}
+
+fn get_power_of_game(chunk: &str) -> u32 {
+    let games = process_chunk(chunk);
+    let mut min_red = 0;
+    let mut min_blue = 0;
+    let mut min_green = 0;
+
+    for game in games {
+        if game.red > min_red {
+            min_red = game.red;
+        }
+        if game.blue > min_blue {
+            min_blue = game.blue;
+        }
+        if game.green > min_green {
+            min_green = game.green;
+        }
+    }
+
+    println!("sum of powers: {}", min_red * min_blue * min_green);
+    min_red * min_blue * min_green
 }
 
